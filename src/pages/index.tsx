a9c6cf185bbf94d58a6854e500d6ace99a85eb65
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-18 21:43:39 -0700

    Socket Start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,39 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import router from "next/router";
+import { v4 as uuid } from "uuid";
 
-export default function Home(): JSX.Element {
-	return <Container></Container>;
+export default function Index(): JSX.Element {
+	const [room, setRoom] = useState("");
+
+	return (
+		<Center h="100vh">
+			<Box>
+				<Input
+					onChange={(e: any) => {
+						const room = e.target.value;
+						setRoom(room);
+					}}
+					placeholder="Room"
+				/>
+
+				<Flex justify="space-between">
+					<Button
+						onClick={() => {
+							router.push(`/play?room=${room}`);
+						}}
+					>
+						Join
+					</Button>
+					<Button
+						onClick={() => {
+							router.push(`/play?room=${uuid()}`);
+						}}
+					>
+						Create Private Match
+					</Button>
+				</Flex>
+			</Box>
+		</Center>
+	);
 }
---
+++ src/pages/play.tsx
@@ -0,0 +1,88 @@
+import { Input, Flex, Box, Center, Text, FormControl } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import { DB_URL } from "config";
+import { getParameterByName } from "@lib/cookie";
+
+const socket = io(DB_URL);
+
+export default function Home(): JSX.Element {
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState([
+		{
+			from: "System",
+			message:
+				"Thank you for creating a session, please wait for one of our therapists to connect to the room",
+		},
+	]);
+
+	type messageType = {
+		from: string;
+		message: string;
+	};
+
+	function displayMessage(displayMessage: string, from: string) {
+		const tempMessages: object[] = [];
+
+		messages.forEach((i: object) => {
+			tempMessages.push(i);
+		});
+
+		tempMessages.push({
+			from: from,
+			message: displayMessage,
+		});
+
+		//@ts-ignore
+		setMessages(tempMessages);
+	}
+
+	useEffect(() => {
+		const room = getParameterByName("room");
+		const role = getParameterByName("role");
+		if (room) socket.emit("join-room", room, role ? true : false);
+		socket.on("message", (msg, role) => {
+			displayMessage(msg, role ? "Therapist" : "Patient");
+		});
+	}, []);
+
+	return (
+		<Center h="100vh">
+			<Box maxW="700px" w="100%" mx="25px">
+				<Box textAlign="left">
+					{messages.map((message: messageType, index: number) => {
+						return (
+							<Text key={index} my="10px">
+								{message.from}:
+								<Text fontSize="sm"> {message.message}</Text>
+							</Text>
+						);
+					})}
+				</Box>
+				<Flex mt="25px">
+					<form
+						onSubmit={(e) => {
+							if (!message) return;
+							e.preventDefault();
+
+							displayMessage(message, "You");
+							setMessage("");
+						}}
+					>
+						<FormControl id="first-name" isRequired>
+							<Input
+								w="100%"
+								placeholder="Message"
+								value={message}
+								onChange={(event: any) => {
+									const temp = event.target.value;
+									setMessage(temp);
+								}}
+							/>
+						</FormControl>
+					</form>
+				</Flex>
+			</Box>
+		</Center>
+	);
+}
