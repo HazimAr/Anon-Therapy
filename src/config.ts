b857f3128d4140b292344de996aaee2a43fdfd39
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-18 23:15:24 -0700

    Revert "Revert "Update config.ts""
    This reverts commit 94d9d23cbe4a2cf78a6f499fcc53af8537839eff.

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const GA_TRACKING_ID = "";
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 const DB_URL = IS_PRODUCTION
-	? "https://b74e5fe69b3e.ngrok.io"
+	? "https://therapy-ses.herokuapp.com"
 	: "http://localhost:6969";
 
 const META = {
