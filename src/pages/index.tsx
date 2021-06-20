90c0c54637c4c7c486074e1d853ea5de711bc5be
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-19 19:32:01 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,31 @@ import Intro from "@components/intro";
 export default function Index(): JSX.Element {
 	return (
 		<Box w="100vw" h="100vh">
-			<Box>
-				<Text
-					fontFamily="Georgia,seriff"
-					fontSize={{ base: "40px", sm: "50px", md: "60px" }}
-				>
-					ANON-THERAPY
-				</Text>
-				<Text
-					fontFamily="Georgia,seriff"
-					fontSize={{ sm: "17px", md: "22px" }}
-					mt="-20px"
-				>
-					—Free Anonymous Online Therapy—
-				</Text>
-				<Flex justify="space-evenly" mt="15px">
-					<Text>HOME</Text> <Text>ABOUT US</Text> <Text>FAQ</Text>
-					<Text>CONTACT US</Text>
-				</Flex>
-				<Divider colorScheme="#6FB7D6" />
-			</Box>
+			<Flex justify="center">
+				<Box maxW="1200px" w="100%">
+					<Text
+						fontFamily="Georgia, seriff"
+						fontSize={{ base: "40px", sm: "50px", md: "60px" }}
+					>
+						ANON-THERAPY
+					</Text>
+					<Text
+						fontFamily="Georgia, seriff"
+						fontSize={{ sm: "17px", md: "22px" }}
+						mt="-20px"
+					>
+						—Free Anonymous Online Therapy—
+					</Text>
+					<Flex justify="space-between" mt="15px" mx="50px">
+						<Text>HOME</Text>
+						<Text>ABOUT US</Text>
+						<Text>FAQ</Text>
+						<Text>CONTACT US</Text>
+					</Flex>
+				</Box>
+			</Flex>
+			<Divider colorScheme="#6FB7D6" />
+
 			{/* <Flex justify="center">
 				<Box>
 					<Image src="/undraw/bluehealth.svg" w="500px" />
