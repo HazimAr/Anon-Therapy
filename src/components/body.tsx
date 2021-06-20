11a39f913c60865df8f3c5ef4bd96b3d236e6443
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-19 21:17:09 -0700

    Update body.tsx

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable no-void */
 /* eslint-disable import/no-default-export */
-import { Box, Flex, Text, Heading } from "@chakra-ui/react";
+import { Box, Flex, Text, Button, Heading } from "@chakra-ui/react";
+import router from "next/router";
+import { v4 as uuid } from "uuid";
 
 export default function Body(): JSX.Element {
 	return (
 		<Flex justify="center">
 			<Flex
-				justify="center"
+				justify="space-evenly"
 				mt="20px"
 				flexWrap="wrap"
 				maxW="1000px"
@@ -15,7 +18,7 @@ export default function Body(): JSX.Element {
 					<Heading size="md" mb="10px">
 						ABOUT US
 					</Heading>
-					<Text ml="">
+					<Text>
 						Welcome to Anon-Therapy, an online therapy center made
 						by a group of students! Our purpose is to help those who
 						want to talk but do not want their identity to be known.
@@ -23,8 +26,17 @@ export default function Body(): JSX.Element {
 						someone so you can speak your thoughts anonymously.
 					</Text>
 				</Box>
-				<Box w="250px" fontSize="25px" mt="40px" ml="auto">
+				<Box w="250px" fontSize="25px" mt="40px">
 					Become a better version of yourself. Request today.
+					<Button
+						mt="20px"
+						onClick={() => {
+							void router.push(`/session?room=${uuid()}`);
+						}}
+						bg="brand.500"
+					>
+						Request Session
+					</Button>
 				</Box>
 			</Flex>
 		</Flex>
