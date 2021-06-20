c3c3f153dd0ff841c67a934c68e929949830f947
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-19 22:28:34 -0700

    Update chat.tsx

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -5,8 +5,13 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-default-export */
-import { Input, Flex, Box, Text, FormControl,Image } from "@chakra-ui/react";
+import { Input, Flex, Box, Text, FormControl, Image } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+	width: 100%;
+`;
 
 export default function Home({ socket, role, room }: any): JSX.Element {
 	const [message, setMessage] = useState("");
@@ -54,25 +59,76 @@ export default function Home({ socket, role, room }: any): JSX.Element {
 	}, [messages]);
 
 	return (
-		<Flex justify="center" h="100vh">
-			<Image src="/logo.png" h="100px" />
-			<Box mx="20px">
-				<Text
-					fontFamily="Georgia, seriff"
-					fontSize={{ base: "40px", sm: "50px", md: "60px" }}
-				>
-					ANON-THERAPY
-				</Text>
-				<Text
-					fontFamily="Georgia, seriff"
-					fontSize={{ sm: "17px", md: "22px" }}
-					mt="-20px"
-					textAlign="center"
-				>
-					—Free Anonymous Online Therapy—
-				</Text>
+		<Flex justify="center" w="100vw" h="100vh">
+			<Box>
+				<Flex maxW="1200px" w="100%" justify="space-between">
+					<Image src="/logo.png" h="100px" />
+					<Box>
+						<Text
+							fontFamily="Georgia, seriff"
+							fontSize={{ base: "40px", sm: "50px", md: "60px" }}
+						>
+							ANON-THERAPY
+						</Text>
+						<Text
+							fontFamily="Georgia, seriff"
+							fontSize={{ sm: "17px", md: "22px" }}
+							mt="-20px"
+							textAlign="center"
+						>
+							—Free Anonymous Online Therapy—
+						</Text>
+					</Box>
+					<Image src="/logo.png" h="100px" />
+				</Flex>
+				<Box maxW="1200px" w="100%" mx="25px">
+					<Flex
+						textAlign="left"
+						h="80vh"
+						flexDir="column"
+						justify="flex-end"
+						overflowY="auto"
+					>
+						{messages.map((message: messageType, index: number) => {
+							return (
+								<Text key={index} my="10px" fontSize="xl">
+									{message.from}:
+									<Text fontSize="lg">
+										{" "}
+										{message.message}
+									</Text>
+								</Text>
+							);
+						})}
+					</Flex>
+
+					<StyledForm
+						onSubmit={(e) => {
+							if (!message) {
+								return;
+							}
+							e.preventDefault();
+							socket.emit("message", message, room);
+							displayMessage(message, "You");
+							setMessage("");
+						}}
+					>
+						<FormControl id="first-name" isRequired>
+							<Input
+								color="text.900"
+								borderColor="#072d75"
+								w="100%"
+								placeholder="Message"
+								value={message}
+								onChange={(event: any) => {
+									const temp = event.target.value;
+									setMessage(temp);
+								}}
+							/>
+						</FormControl>
+					</StyledForm>
+				</Box>
 			</Box>
-			<Image src="/logo.png" h="100px" />
 		</Flex>
 		// <Flex h="100vh" align="flex-end">
 		// 	<Box maxW="2000px" w="100%" mx="25px">
