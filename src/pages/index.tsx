e0c13e4af75cd5435389c8d05597209d3e05520d
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-19 17:34:15 -0700

    Spacing Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,28 +5,36 @@ import { v4 as uuid } from "uuid";
 
 export default function Index(): JSX.Element {
 	return (
-		<Flex justify="center" align="center">
-			<Box maxW="450px" textAlign="left">
-				<Box>
-					<Heading>Anonymous Therapy Done Right</Heading>
-					<Text mt="10px" color="text.400">
-						Lorem ipsum dolor, sit amet consectetur adipisicing
-						elit. Magni qui veniam possimus quod, laudantium,
-						adipisci doloribus at debitis quam dolorem tempora.
-					</Text>
+		<Flex justify="center">
+			<Flex
+				justify="center"
+				align="center"
+				flexWrap="wrap"
+				maxW="1200px"
+				mx="25px"
+			>
+				<Box maxW="450px" textAlign="left" my="20px">
+					<Box>
+						<Heading>Anonymous Therapy Done Right</Heading>
+						<Text my="10px" color="text.400">
+							Lorem ipsum dolor, sit amet consectetur adipisicing
+							elit. Magni qui veniam possimus quod, laudantium,
+							adipisci doloribus at debitis quam dolorem tempora.
+						</Text>
+					</Box>
+					<Button
+						mt="20px"
+						onClick={() => {
+							void router.push(`/session?room=${uuid()}`);
+						}}
+					>
+						Request Session
+					</Button>
 				</Box>
-				<Button
-					mt="25px"
-					onClick={() => {
-						void router.push(`/session?room=${uuid()}`);
-					}}
-				>
-					Request Session
-				</Button>
-			</Box>
-			<Box>
-				<Image src="/undraw/doctors.svg" w="500px" />
-			</Box>
+				<Box my="20px">
+					<Image src="/undraw/doctors.svg" w="500px" />
+				</Box>
+			</Flex>
 		</Flex>
 	);
 }
